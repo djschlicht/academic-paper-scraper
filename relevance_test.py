88ea8ea60fada1f0f03a5af45fc5ebd9b909c64c
Dan Schlicht <dan.schlicht@gmail.com>
2020-10-26 13:56:01 -0400

    fixed problem with bag of words

--- relevance_test.py
+++ relevance_test.py
@@ -25,6 +25,24 @@ traits = [	# traits
 	"interspecies transmission", "pathogenicity", "virulence"
 	]
 
+bag = [	# traits
+	"first detected", "first recorded outbreak", "first outbreak", "outbreak",
+	"geographic", "location", "origin", "human transmission",
+	"asymptomatic", "incubation period", "infectious period", "latent period",
+	"fatality rate", "mortality rate", "pre-symptomatic infection", "human-to-human",
+	"animal-to-human", "vector", "reservoir", "host species",
+	"interspecies transmission", "pathogenicity", "virulence",
+	# diseases
+	"ebola", "EVD", "zika", "ZIKV", "nipah","NiV","chikungunya", "CHIKV", "hendra",
+	"marburg", "MVD", "MARV", "dengue", "DENV", "lassa", "Lujo", "Lusaka/Johannesburg",
+	"Lyme disease", "Lyme borreliosis","West nile", "heartland virus", "HRTV", 
+	"Severe fever with thrombocytopenia syndrome", "SFTS", "St. Louis virus",
+	"Powassan virus", "POWV", "deer tick virus", "DTV", "Australian bat lyssavirus",
+	"ABLV", "H1N1", "swine flu", "pig flu", "MERS", "middle east respiratory syndrome",
+	"avian flu", "bird flu", "avian influenza", "HIV", "human immunodeficiency virus",
+	"SARS", "SARS-COV", "severe acute respiratory syndrome", "Covid", "SARS-COV-2", "novel coronavirus",
+	]
+	
 # deletes files that are empty or title only
 def delete_if_empty(filename):
 	size = os.path.getsize(f)
@@ -41,7 +59,7 @@ def relevance_check(filename):
 	with open(filename, "r") as f:
 		title = f.readline()
 		for line in f:
-			for elem in traits:
+			for elem in bag:
 				if elem in line:
 					count += 1
 		
