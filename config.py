3ff7afad1fe4f0cb924f4f8f3f15480fd4559280
Dan Schlicht <dan.schlicht@gmail.com>
2020-11-19 02:55:00 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,2 @@
 # Springer API Key
-springer_api_key = '923da2a07addcdc0fa872ffddeebae78'
-
-# Elsevier/ScienceDirect API Key
-elsevier_api_key = 'bbb99e6c74c927c82d275434494ec2ad'
-
+springer_api_key = 'YOURAPIKEYHERE'
