c163d05ecf4693b6061f9cac698be0ec4f5bc7a2
Dan Schlicht <dan.schlicht@gmail.com>
2021-03-05 17:17:27 -0500

    put doi citation getter into function

--- citations.py
+++ citations.py
@@ -1,58 +1,55 @@
-#!/usr/bin/env python3
-# citations.py - pulls citations from article urls
-
-'''
-Warning: This will take a long time to run depending on how the 
-		 citation servers feel on any given day.
-'''
+# citations.py
+# Cite papers from their URL
 
 import pandas as pd
 import requests
 import os
 
-# get working dir
-cwd = os.getcwd()
-
-# specify what kind of citation to get
-headers = {'Accept': 'text/x-bibliography; style=apa'}
-
-# get the article doi-urls
-df = pd.read_csv(cwd+'/database/db.csv', usecols=["Article URL"])
-
-txt = ''
-cur_line = 2 # first row is headers and csv usually not 0 indexed
-cited = {} # dictionary for if one source is found multiple times
-# get the citations for each article and store them in a string to be written to a file
-for doi in df['Article URL']:
-	if type(doi) is not str:
-		continue
-		cur_line += 1
-	else:
-		# if the source is already cited, just copy it
-		if doi in cited:
-			txt += str(cur_line) + '\n'
-			txt += cited[doi] + '\n'
-			cur_line += 1 
-			continue
-			
-		# if it's not already cited, request the citation from the server
-		while(True):
-			try:
-				response = requests.get(doi, headers=headers)
-				response.encoding = 'utf-8'
-				print(str(cur_line) + ': ' + str(response.status_code))
-				if(response.status_code == 200):
-					break
-			except Exception as inst:
-				print(inst)
-		# add the citation to the text string and increment cur_line
-		txt += str(cur_line) + '\n'
-		txt += str(response.text) + '\n\n'
-		cited[doi] = response.text
-		cur_line += 1
-
-# write the citations to a file
-with open(cwd+'/database/citations.txt', 'w+') as cites:
-	cites.write(txt)
-
+def cite_doi(path, column):
+	"""Write file containing citations from all rows with DOI link
 	
+	Arguments:
+	path	-- the path to the csv (e.g. '../data.csv')
+	column	-- name of column with urls
+	"""
+	
+	# specify what kind of citation - sent to doi citation server
+	headers = {'Accept': 'text/x-bibliography; style=apa'}
+	df = pd.read_csv(path, usecols=[column])
+	txt = ''
+	cur_row = 2 # csv is 1 indexed and first line is col names
+	cited = {}
+
+	for doi in df['URL']:
+		if "doi.org" not in doi:
+			continue
+			cur_row += 1
+			
+		else:
+			# pull duplicates from cited
+			if doi in cited:
+				txt += str(cur_row) + '\n'
+				txt += cited[doi] + '\n'
+				cur_row += 1 
+				continue
+
+			while(True):
+				try:
+					response = requests.get(doi, headers=headers)
+					response.encoding = 'utf-8'
+					# only add to citations if server responds
+					if(response.status_code == 200):
+						break
+				except Exception as inst:
+					print(inst)
+
+			txt += str(cur_line) + '\n'
+			txt += str(response.text) + '\n\n'
+			cited[doi] = response.text
+			cur_line += 1
+
+	cwd = os.getcwd()
+	with open(cwd + 'citations.txt', 'w+') as citations:
+		citations.write(txt)
+
+# TODO: add functions for other document ID types (PMID, etc)
